bcc26bc0e713506437ebf3183713872bfb8ee909
felipe421 <felipe.arcanjo@iesb.edu.br>
2024-04-26 21:55:11 -0300

    formas_access

---
+++ app/models/produto.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Produto extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare nome: String
+
+  @column()
+  declare preco: Number
+
+  @column()
+  declare tamanho: String
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
