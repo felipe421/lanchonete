a0e1e4dce23f2c415f00367a43f5cb8ccae13333
felipe421 <felipe.arcanjo@iesb.edu.br>
2024-05-03 21:47:52 -0300

    oi

--- start/routes.ts
+++ start/routes.ts
@@ -11,6 +11,7 @@ import router from '@adonisjs/core/services/router'
 import ProdutosController from '../app/controllers/produtos_controller.js'
 import ClientesController from '../app/controllers/clientes_controller.js'
 import FuncionariosController from '../app/controllers/funcionarios_controller.js'
+import ComandasController from '../app/controllers/comandas_controller.js'
 
 router.get('/', async () => {
   return {
@@ -26,5 +27,6 @@ router.get('/hello', async () => {
 
 router.resource('/felipe ', ProdutosController).apiOnly()
 router.resource('/felipe ', ClientesController).apiOnly()
+router.resource('/felipe ', FuncionariosController).apiOnly()
 router.resource('/felipe ', FuncionariosController).apiOnly()
 
